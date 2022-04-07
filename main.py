5c7692529ae09a1cbb946987aabb013d02b9ca20
Valera <valera.kkoz2@gmail.com>
2022-04-07 14:53:10 +0300

    added user

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ class Book:
 
     def __str__(self):
         return f'{self.id} - "{self.name}" ({self.author})'
+
+class User:
+    def __init__(self, id: int, name: str, surname: str, age: int):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def __str__(self):
+        return f'{self.id} - {self.name} {self.surname}'
