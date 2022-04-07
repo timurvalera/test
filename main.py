6b2ec1c9caa0ddc83bf49fb194cf8d57ca6aa56c
Светлана <smirnovasvet47@gmail.com>
2022-04-07 14:31:34 +0300

    added book class

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from typing import List
+
+
+class Book:
+    def __init__(self, id: int, name: str, pages: int, author: str):
+        self.id = id
+        self.name = name
+        self.pages = pages
+        self.author = author
+        self.owner = None
+
+    def __str__(self):
+        return f'{self.id} - "{self.name}" ({self.author})'
